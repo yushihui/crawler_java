7186ef2f2463b4e19a74a746e1fbb2e1e68eb3da
shihui <yushihui@gmail.com>
2017-03-21 18:31:48 -0400

    add service manager

---
+++ src/main/java/com/wj/crawler/scheduler/SchedulerServiceManager.java
@@ -0,0 +1,35 @@
+package com.wj.crawler.scheduler;
+
+import com.google.common.util.concurrent.Service;
+import com.google.common.util.concurrent.ServiceManager;
+
+import javax.inject.Singleton;
+
+/**
+ * Created by Administrator on 3/21/2017.
+ */
+
+@Singleton
+public class SchedulerServiceManager {
+
+    private ServiceManager serviceManager;
+
+    public SchedulerServiceManager(Iterable<? extends Service> services){
+        serviceManager = new ServiceManager(services);
+    }
+
+    public ServiceManager getInstance(){
+
+        return serviceManager;
+    }
+
+    public void stop(){
+        this.serviceManager.stopAsync();
+    }
+
+    public void start(){
+        this.serviceManager.startAsync();
+    }
+
+
+}
---
+++ src/main/java/com/wj/crawler/scheduler/WeiboFetchService.java
@@ -0,0 +1,45 @@
+package com.wj.crawler.scheduler;
+
+import com.google.common.util.concurrent.AbstractScheduledService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 3/21/2017.
+ */
+public class WeiboFetchService extends AbstractScheduledService {
+
+    private static final Logger Log = LoggerFactory.getLogger(WeiboFetchService.class);
+
+    @Inject
+    private Properties config;
+
+    private int hours = 1;
+
+
+    protected void startUp() throws Exception {// get
+        hours = Integer.parseInt(config.getProperty("weibo.freq", "6"));
+    }
+
+    protected void runOneIteration() throws Exception {
+
+    }
+
+    protected void shutDown() throws Exception {
+
+    }
+
+    protected Scheduler scheduler() {
+
+        return Scheduler.newFixedRateSchedule(0, hours, TimeUnit.HOURS);
+    }
+
+    protected String serviceName() {
+        return "Fetch_Weibo_Content_Service";
+    }
+
+}
