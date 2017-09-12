853aaf2000ed922632a9592ebef7423cf0acd809
SHIHUI YU <yushihui@gmail.com>
2017-09-12 18:09:31 -0400

    Update SummaryAnalyze.java

--- src/main/java/com/shihui/nlp/SummaryAnalyze.java
+++ src/main/java/com/shihui/nlp/SummaryAnalyze.java
@@ -34,7 +34,7 @@ public class SummaryAnalyze {
 
     public static void main(String rags[]) {
         Segment nShortSegment = new NShortSegment().enableCustomDictionary(false).enablePlaceRecognize(true).enableOrganizationRecognize(true);
-        List<Term> res = nShortSegment.seg("你好，欢迎使用HanLP汉语处理包！ hEllo");
+        List<Term> res = nShortSegment.seg("你好，欢迎使用hanLP汉语处理包！ HEllo");
         new SummaryAnalyze().readFile();
     }
 
